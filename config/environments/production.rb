bcb711744d9ef8433dbcaf5ab458a8b8eda16752
Justin Raczak <jraczak@gmail.com>
2017-07-09 15:29:18 -0700

    Set default_url_options host in production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => "pure-caverns-40977.herokuapp.com" }
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
